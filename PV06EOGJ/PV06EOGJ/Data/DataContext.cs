fd85b7b684b93837cae193a72658fdc67ddf786c
OmarGanem <126487606+OmarGanem@users.noreply.github.com>
2023-03-01 09:45:30 -0600

    Add files via upload

---
+++ PV06EOGJ/PV06EOGJ/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Data.Entity;
+
+namespace PV06EOGJ.Data
+
+{
+    public class DataContext: DbContext
+    {
+        DbSet<equipo> Equipos { get; set; }
+        public DataContext():base(¨name=con¨) 
+        { }
+    }
+}
---
+++ PV06EOGJ/PV06EOGJ/Data/equipo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PV06EOGJ.Data
+{
+    public class equipo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string city { get; set; }
+        public string website { get; set; }
+        public string date { get; set; }
+    }
+}
